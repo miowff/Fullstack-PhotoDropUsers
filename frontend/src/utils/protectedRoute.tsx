55f346a9642131fb563cba83dbcf42a763495bf9
Mykola <95630245+miowff@users.noreply.github.com>
2023-11-02 19:05:43 +0200

    protected route fixes

--- frontend/src/utils/protectedRoute.tsx
+++ frontend/src/utils/protectedRoute.tsx
@@ -2,5 +2,6 @@ import { Navigate } from "react-router-dom";
 
 export const PrivateWrapper = ({ children }: { children: JSX.Element }) => {
   const token = localStorage.getItem("ACCESS_TOKEN");
-  return token ? children : <Navigate to="/start" replace />;
+
+  return token !== null ? children : <Navigate to="/start" replace />;
 };
